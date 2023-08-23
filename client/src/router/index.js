863283cce4bfbe183c00c23b986d3000526fdfe1
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-08-23 09:47:43 +0300

    Move client's files to the client folder

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '../views/About'
-import Home from '../views/Home'
+import About from '../views/About.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
